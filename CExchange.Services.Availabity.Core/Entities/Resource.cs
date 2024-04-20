a83431f62bf5f1fa009f758933932a4c4047585d
Michał Łęski <Mleski1997@gmail.com>
2024-04-20 14:33:27 +0200

    added avability.core

---
+++ CExchange.Services.Availabity.Core/Entities/Resource.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CExchange.Services.Availabity.Core.Entities
+{
+    internal class Resource
+    {
+    }
+}
---
+++ CExchange.Services.Availabity.Core/ValueObjects/Reservation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CExchange.Services.Availabity.Core.ValueObjects
+{
+    public struct Reservation : IEquatable<Reservation>
+    {
+        public DateTime DateTime { get;  }
+        public int Prioryty { get; }
+
+        public Reservation(DateTime dateTime, int priority)
+        {
+            DateTime = dateTime;
+            Prioryty = priority;
+            
+        }
+
+        public bool Equals(Reservation other)
+        {
+            return DateTime.Equals(other.DateTime) && Prioryty == other.Prioryty;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Reservation other && Equals(other);  
+        }
+
+        public override int GetHashCode() => HashCode.Combine(DateTime, Prioryty);
+
+    }
+}
